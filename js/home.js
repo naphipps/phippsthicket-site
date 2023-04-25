645d7dcf76e5fd3b1a48d657a02147814bfd55ed
Nathan Phipps <naphipps@noreply.com>
2023-04-25 10:05:50 -0500

    minor removed console log

--- js/home.js
+++ js/home.js
@@ -1,8 +1,6 @@
 var home = new function() {
 
     function init(){
-        console.log("hello world from home")
-
         util.load("html/under_construction.html", (files)=>{
             util.byId("home_latest_news").innerHTML = files["html/under_construction.html"];
         });
