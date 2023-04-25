8030b10b5aefa96ee4b15ef08ef17295066cfccb
Nathan Phipps <naphipps@noreply.com>
2023-04-25 18:07:32 -0500

    updated home

--- js/home.js
+++ js/home.js
@@ -7,8 +7,11 @@
 var home = new function() {
 
     function init(){
+        util.byId("content").style.display = "none";
+
         util.load("html/under_construction.html", (files)=>{
             util.byId("home_latest_news").innerHTML = files["html/under_construction.html"];
+            util.byId("content").style.display = "block";
         });
 
         util.byId("home_about_button").addEventListener("click", ()=>{index.openPage("About");});
